ca438c45a7b866ae9c0f0d58671770a8bebf36a9
EC2 Default User <ec2-user@ip-172-31-70-51.ec2.internal>
2018-03-10 18:20:05 +0000

    Student model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create([{
+    uin: 123000123,
+    name: 'Ruth Morris',
+    section: 500,
+    attempts: 0,
+    score: -1,
+    last_start: DateTime.strptime("03/10/2018 17:00", "%m/%d/%Y %H:%M"),
+    last_end: DateTime.strptime("03/10/2018 20:00", "%m/%d/%Y %H:%M")
+}])

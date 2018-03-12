260f05cc588734f792ea4f568c7503dd2beb13d7
EC2 Default User <ec2-user@ip-172-31-9-44.ec2.internal>
2018-03-12 03:38:10 +0000

    Update database config

---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180310173153) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.integer "uin"
+    t.string "name"
+    t.integer "section"
+    t.integer "attempts"
+    t.integer "score"
+    t.datetime "last_start"
+    t.datetime "last_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uin"], name: "index_students_on_uin"
+  end
+
+end

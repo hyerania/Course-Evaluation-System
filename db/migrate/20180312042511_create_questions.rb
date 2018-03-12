edc1f73086a1c47e7932eee9c0d84bb2d9de8d0a
EC2 Default User <ec2-user@ip-172-31-29-151.ec2.internal>
2018-03-12 04:26:15 +0000

    Fixed index on db for question

--- db/migrate/20180312042511_create_questions.rb
+++ db/migrate/20180312042511_create_questions.rb
@@ -1,13 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
-      t.integer :id
+      t.integer :qid
       t.string :content
       t.string :type
       t.string :answer
 
       t.timestamps
     end
-    add_index :questions, :id
+    add_index :questions, :qid
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310173153) do
+ActiveRecord::Schema.define(version: 20180312042511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "qid"
+    t.string "content"
+    t.string "type"
+    t.string "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["qid"], name: "index_questions_on_qid"
+  end
 
   create_table "students", force: :cascade do |t|
     t.integer "uin"

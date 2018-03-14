a53e52c77dca35923a4dcebd8dabd0d9d327a67b
hyerania <hernandez.yerania2@gmail.com>
2018-03-14 23:02:17 +0000

    Attempt to show table

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
+   
+  def student_params
+    params.require(:student).permit(:uin, :name, :section, :attempts, :score)
+  end
+
+  def show
+    uin = params[:uin] # retrieve student ID from URI route
+    @student = Students.find(uin) # look up movie by unique ID
+  end
+  
   def show
   end
 end

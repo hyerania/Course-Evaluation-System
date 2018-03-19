673f48ec80d70420ddfbfc38e2b4523198dc9c71
EC2 Default User <ec2-user@ip-172-31-3-73.us-east-2.compute.internal>
2018-03-19 19:48:25 +0000

    Iteration 1 (Siru + Amulya)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
   def show
     # uin = params[:uin] # retrieve student ID from URI route
     @students = Student.all # look up movie by unique ID
-    
+    @average=Student.all.average(:score)
   end
   
 end

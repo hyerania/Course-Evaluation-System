e601bbaa590cf73d7cc01f9ad2f388c8214008ae
hyerania <hernandez.yerania2@gmail.com>
2018-03-19 13:56:49 +0000

    Changed admin view to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
   def show
+    # uin = params[:uin] # retrieve student ID from URI route
+    @students = Student.all # look up movie by unique ID
+    
   end
 end

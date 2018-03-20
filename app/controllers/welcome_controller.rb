372e790446561df7f6f0527215027d571a750e3a
Kevin Nguyen <kevinjnguyen2@gmail.com>
2018-03-20 01:23:00 +0000

    Updated welcome view to original splash.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,8 +5,6 @@ class WelcomeController < ApplicationController
   # end
 
   def show
-    # uin = params[:uin] # retrieve student ID from URI route
-    #@students = Student.all # look up movie by unique ID
     
   end
   

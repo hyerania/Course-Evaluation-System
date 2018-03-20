dccdba333ae781f4b556e61d51d7a525677c762d
EC2 Default User <ec2-user@ip-172-31-23-250.us-east-2.compute.internal>
2018-03-20 00:40:39 +0000

    Course-evaluation-results done

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
   def show
     # uin = params[:uin] # retrieve student ID from URI route
     @students = Student.all # look up movie by unique ID
-    
+    @average=Student.all.average(:score)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,11 @@ class WelcomeController < ApplicationController
   # def student_params
   #   params.require(:student).permit(:uin, :name, :section, :attempts, :score)
   # end
+
   def show
     # uin = params[:uin] # retrieve student ID from URI route
-    @students = Student.all # look up movie by unique ID
-    @average=Student.all.average(:score)
+    #@students = Student.all # look up movie by unique ID
+    
   end
   
 end

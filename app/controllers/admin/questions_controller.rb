2ea76b569329c737be3efc0ad6686099f0847c0e
Kevin Nguyen <kevinjnguyen2@gmail.com>
2018-03-27 03:17:20 +0000

    Updated to auto-increment

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -5,7 +5,18 @@ class Admin::QuestionsController < ApplicationController
   end
   
   def create
-    @question = Question.create!(question_params)
+    @all_questions = Question.all
+    last_question =  @all_questions.last
+    
+    @question = Question.new
+    @question.qid = last_question.qid + 1
+    @question.content = question_params[:content]
+    @question.c1 = question_params[:c1]
+    @question.c2 = question_params[:c2]
+    @question.c3 = question_params[:c3]
+    @question.c4 = question_params[:c4]
+    @question.answer = question_params[:answer]
+    @question.save
     redirect_to action: "show"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312142612) do
+ActiveRecord::Schema.define(version: 20180327024538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
-    t.integer "qid"
+    t.integer "qid", null: false
     t.string "content"
     t.string "qtype"
     t.string "answer"

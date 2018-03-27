a6740d68e9f8d9a452228f9dde93dee29f14e864
EC2 Default User <ec2-user@ip-172-31-3-73.us-east-2.compute.internal>
2018-03-27 02:17:54 +0000

    links setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
 Rails.application.routes.draw do
   get 'admin/show'
 
+  get 'welcome/show'
+
+  get 'students/hello'
+  
   get 'students/show'
 
-  resources :students
   get 'questions/view'
 
   get 'questions/edit'
 
   get 'questions/remove'
-
-  #root to: 'welcome#show'
-  root to: 'students#hello'
+  
+  root to: 'welcome#show'
+  #root to: 'students#hello'
   get 'about', to: 'about#show'
   #Yang: just for testing
   get 'admin', to: 'admin#show'
   # '/' Route
-  get 'welcome/show'
-  get 'students/hello'
+  
+
+  resources :students
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

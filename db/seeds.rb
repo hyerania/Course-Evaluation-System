d915f3f52236a044be50e51ed84ff88c568b9a18
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2018-04-03 03:26:18 +0000

    Fixed seed issue for access code creation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,6 +35,6 @@ Question.create([{
     {qid: 1006, content: "What is the color of the blood3?", qtype: "ML", answer: "Green", c1: "Blue", c2: "Green", c3: "Brown", c4: "Violet", c5: "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")}
 ])
 
-AccessCode.Create([{
+AccessCode.create([{
     code: "CSCE120-2018"
 }])

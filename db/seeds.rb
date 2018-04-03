82c3309b76d54e8d2fda1d6548c26344d8a1ca39
EC2 Default User <ec2-user@ip-172-31-95-220.ec2.internal>
2018-04-03 02:00:22 +0000

    db fix2

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,11 +28,11 @@ Question.create([{
     c3: "White",
     c4: "Pink",
     c5: "Orange"},
-    {qid => 1002, content => "What is the color of the grass?", qtype => "ML", answer => "Green", c1 => "Blue", c2 => "Green", c3 => "Brown", c4 => "Violet", c5 => "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
-    {qid => 1003, content => "What is the color of the blood?", qtype => "ML", answer => "Green", c1 => "Blue", c2 => "Green", c3 => "Brown", c4 => "Violet", c5 => "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
-    {qid => 1004, content => "What is the color of the hair?", qtype => "ML", answer => "Green", c1 => "Blue", c2 => "Green", c3 => "Brown", c4 => "Violet", c5 => "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
-    {qid => 1005, content => "What is the color of the blood?", qtype => "ML", answer => "Green", c1 => "Blue", c2 => "Green", c3 => "Brown", c4 => "Violet", c5 => "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
-    {qid => 1006, content => "What is the color of the blood?", qtype => "ML", answer => "Green", c1 => "Blue", c2 => "Green", c3 => "Brown", c4 => "Violet", c5 => "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")}
+    {qid: 1002, content: "What is the color of the grass?", qtype: "ML", answer: "Green", c1: "Blue", c2: "Green", c3: "Brown", c4: "Violet", c5: "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
+    {qid: 1003, content: "What is the color of the blood?", qtype: "ML", answer: "Green", c1: "Blue", c2: "Green", c3: "Brown", c4: "Violet", c5: "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
+    {qid: 1004, content: "What is the color of the hair?", qtype: "ML", answer: "Green", c1: "Blue", c2: "Green", c3: "Brown", c4: "Violet", c5: "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
+    {qid: 1005, content: "What is the color of the blood2?", qtype: "ML", answer: "Green", c1: "Blue", c2: "Green", c3: "Brown", c4: "Violet", c5: "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")},
+    {qid: 1006, content: "What is the color of the blood3?", qtype: "ML", answer: "Green", c1: "Blue", c2: "Green", c3: "Brown", c4: "Violet", c5: "Red", created_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M"),updated_at: DateTime.strptime("03/24/2018 17:00", "%m/%d/%Y %H:%M")}
 ])
 
     

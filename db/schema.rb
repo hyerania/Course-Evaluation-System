2326dd5a8803e6bc5f1e8717486f436047cfa8ab
EC2 Default User <ec2-user@ip-172-31-95-220.ec2.internal>
2018-04-29 21:30:51 +0000

    update question and evaluation view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426053843) do
+ActiveRecord::Schema.define(version: 20180427004140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,18 +2,6 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
     describe "POST #goback" do
-        before(:each) do
-            #visit "/"
-            #fill_in :uin, with: 123000123
-            #find('submit_tag "Login"').click
-            #session[:uin]=123000213
-        end
-        it "trigger goback" do
-            @student=Student.all.first
-            @evaluation=["What is the color of the blood2?"]
-            
-            get :view, :params => {:page => "2",:stat => "noanswer",:commit => "Next"}
-            expect(response).to have_http_status(302)
-        end
+        
     end
 end
